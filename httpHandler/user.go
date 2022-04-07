b1ea9796b601ed7ca9ccfd2a282c6a68cef8ad1a
深蓝 <mail@ri-co.cn>
2022-04-07 17:04:38 +0800

    新增 用户登录handler，未完成cookie

--- httpHandler/user.go
+++ httpHandler/user.go
@@ -1,9 +1,28 @@
 package httphandler
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/Sirlanri/distiot-user-server/server/log"
+	"github.com/Sirlanri/distiot-user-server/server/user"
+	"github.com/kataras/iris/v12"
+)
 
 //handler 处理登录请求
 func LoginHandler(con iris.Context) {
+	var userData UserLoginData
+	err := con.ReadJSON(&userData)
+	if err != nil {
+		log.Log.Infoln("httpHandler LoginHandler 读取json失败", err.Error())
+		con.StatusCode(401)
+		return
+	}
+	if user.UserLogin(userData.Mail, userData.Pw) {
+
+		con.StatusCode(200)
+	} else {
+		con.StatusCode(401)
+		log.Log.Infoln("httpHandler LoginHandler 登录失败", err.Error())
+		return
+	}
 
 }
 
@@ -15,4 +34,10 @@ func RegisterHandler(con iris.Context) {
 //handler 处理重置密码请求
 func ResetPW(con iris.Context) {
 
+}
+
+//用户登录数据
+type UserLoginData struct {
+	Mail string `json:"mail"`
+	Pw   string `json:"pw"`
 }
