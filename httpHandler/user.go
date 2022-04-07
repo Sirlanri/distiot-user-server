34df20579e4191f7d24ec0eb23c5c8ee0c4322a4
深蓝 <mail@ri-co.cn>
2022-04-07 10:28:22 +0800

    修改 添加handler注释

--- httpHandler/user.go
+++ httpHandler/user.go
@@ -2,14 +2,17 @@ package httphandler
 
 import "github.com/kataras/iris/v12"
 
+//handler 处理登录请求
 func LoginHandler(con iris.Context) {
 
 }
 
+//handler 处理注册请求
 func RegisterHandler(con iris.Context) {
 
 }
 
+//handler 处理重置密码请求
 func ResetPW(con iris.Context) {
 
 }
